895539d24e576b9eb4465bdf3cf07cbb5b10391e
Raul Filea <74644345+RaulFilea@users.noreply.github.com>
2022-06-06 18:04:10 +0300

    Delete Time_To_Smile/HTML Files directory

--- Time_To_Smile/HTML Files/script.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-  let email = document.forms["checkAppointments"]["emailAdress"].value;
-  if (email == "") {
-    alert("Please fill in the email address");
-    return false;
-  }
-}
